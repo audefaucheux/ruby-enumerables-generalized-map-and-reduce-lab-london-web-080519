810af3896d8fc6605bdef1cfecf9c347164be9b8
Aude Faucheux <aude53@gmail.com>
2019-07-25 19:36:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,11 +6,12 @@ def map (array)
   i = 0
   while i < array.length do
     negative.push(array[i] * -1)
-    array
     doubled.push(array[i] * 2)
     squared.push(array[i] ** 2)
     i += 1
   end
+  puts negative
+  puts array
 end
 
 def reduce
