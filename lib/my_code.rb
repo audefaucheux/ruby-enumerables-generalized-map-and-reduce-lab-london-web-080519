1cb68722ef983fa6b44f9a41cb94930825582a47
Aude Faucheux <aude53@gmail.com>
2019-07-25 20:38:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,23 +1,11 @@
 # Your Code Here
 def map(array)
-  yield array
-    #negative.push(array[i] * -1)
-    #doubled.push(array[i] * 2)
-    #squared.push(array[i] ** 2)
-  return array
-end
-
-map([1, 2, 3, -9]) do |array|
   i = 0
   new = []
+  n = 0
   while i < array.length do
-    array[i] * -1
-    i += 1
+    push.new(array[i] + yield n)
   end
-end
-
-map(["paul", "gurney", "vladimir", "jessica", "chani"]) do |array|
-  array
 end
 
 def reduce
