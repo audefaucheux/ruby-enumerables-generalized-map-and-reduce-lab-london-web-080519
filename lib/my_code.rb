339739cfa0a0e7e6332244d5e0501436fd2b07b0
Aude Faucheux <aude53@gmail.com>
2019-07-25 21:38:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,11 @@ end
 
 def reduce(array, starting_point = nil)
   i = 0
-  new = starting_point
+
+  if starting_point
+    new = starting_point
+  else new = nil
+  end
 
   while i < array.length do
     new = yield(new, array[i])
