e383ebed2bc306127f74e6705c5bfa75c3a981a3
Aude Faucheux <aude53@gmail.com>
2019-07-25 20:14:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,16 +1,18 @@
 # Your Code Here
-def map (array)
-  negative = []
-  doubled = []
-  squared = []
+def map(array)
   i = 0
   while i < array.length do
-    negative.push(array[i] * -1)
-    doubled.push(array[i] * 2)
-    squared.push(array[i] ** 2)
+    yield i
+    #negative.push(array[i] * -1)
+    #doubled.push(array[i] * 2)
+    #squared.push(array[i] ** 2)
     i += 1
   end
   return negative
+end
+
+map([1, 2, 3, -9]) do |i|
+  puts [1, 2, 3, -9] * -1
 end
 
 def reduce
