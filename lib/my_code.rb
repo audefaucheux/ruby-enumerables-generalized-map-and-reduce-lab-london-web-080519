ddc49e96c57f30e475e7fe1dc4743e6f837e2c97
Aude Faucheux <aude53@gmail.com>
2019-07-25 19:34:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,10 +3,12 @@ def map (array)
   new = []
   i = 0
   while i < array.length do
-    new.push(array[i] * -1)
+    negative.push(array[i] * -1)
+    array
+    doubled.push(array[i] * 2)
+    squared.push(array[i] ** 2)
     i += 1
   end
-  return new
 end
 
 def reduce
