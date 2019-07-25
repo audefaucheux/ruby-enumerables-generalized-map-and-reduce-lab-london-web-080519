a78d8f9145ab520e218e9722cd4d713a1df9c908
Aude Faucheux <aude53@gmail.com>
2019-07-25 20:59:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,8 @@ def reduce(array, starting_point = 0)
   i = 0
   new = starting_point
   while i < array.length do
-    new += yield(array[i])
+    new += array[i]
+    yield(new, i)
     i += 1
   end
   return new
